02214eccc40c5d19dc5b8c3182e4fca4695567a6
Aaron Decker <d00ble.ard@gmail.com>
2013-08-04 00:21:35 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-## Very important:
-# if you run this from cron, set the encoding !!!
-PYTHONIOENCODING=utf8
+## Python 2.x has issues with unicode running from cron sometimes...
+# if you run this from cron, set the default encoding.
+export PYTHONIOENCODING=utf-8
 python /opt/ssms/server.py
